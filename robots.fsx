24f6557bed65e6b93a8cf4c48671a5fc311e147b
Henrik Christensen <henrikdchristensen@gmail.com>
2021-12-16 15:32:30 +0100

    the big commit for all coders in the world be ready!

--- robots.fsx
+++ robots.fsx
@@ -1,14 +1,18 @@
 type BoardDisplay(rows:int, cols:int) =
-    let mutable board = Array2D.create (rows*2) (cols*2) "-"
-    member this.Set((row:int),(col:int)) = board.[row*2,col*2] <- "1"
+    let mutable board = Array2D.create (rows*2+1) (cols*2+1) "-"
+    member this.Set((row:int),(col:int)) = board.[row*2-1,col*2-1] <- "1"
     member this.Show() = 
         let mutable str = ""
-        for row=0 to (rows*2)-1 do
-            for col=0 to (cols*2)-1 do
-                str <- str + board.[row, col]
+        for row=0 to rows*2 do
+            for col=0 to cols*2 do
+                str <- str + board.[row,col]
             str <- str + "\n"
         printfn "%s" str
 
-let test1 = new BoardDisplay(2,2)
+let test1 = new BoardDisplay(3,3)
 test1.Set(1,1)
+test1.Show()
+test1.Set(2,2)
+test1.Show()
+test1.Set(1,2)
 test1.Show()
