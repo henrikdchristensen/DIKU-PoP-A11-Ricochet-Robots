5445259757a6bab5bce878a7111ccc1eadcd05c2
Henrik Christensen <henrikdchristensen@gmail.com>
2021-12-21 15:00:30 +0100

    commit test igen igen

--- robots.fsx
+++ robots.fsx
@@ -25,8 +25,6 @@ test1.Set(2,1,"1")
 test1.SetRightWall(2,2)
 test1.Show()
 
-
-//HEJ
 type Direction = North | South | East | West
 type Position = int * int
 
@@ -35,6 +33,4 @@ type Action =
     | Continue of Direction * Position
     | Ignore
 
-
-
-//HEJ ASKE
+// HEJ IGEN
