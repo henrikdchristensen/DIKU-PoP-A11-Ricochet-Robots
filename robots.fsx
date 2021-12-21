c47c622059195495bf06c8932d694e5fdc3cd840
a-lundsgaard <askelundsgaard@live.dk>
2021-12-21 11:33:34 +0100

    Finish BoardDisplay

--- robots.fsx
+++ robots.fsx
@@ -1,20 +1,37 @@
 type BoardDisplay(rows:int, cols:int) =
-    let mutable board = Array2D.create (rows*2+1) (cols*2+1) "-"
-    member this.Set((row:int),(col:int),(cont:string)) = board.[row*2-1,col*2-1] <- cont
-    //member this.SetBottomWall((row:int),(col:int)) =
-    //member this.SetRightWall((row:int,(col:int)) =
+    let mutable board = Array2D.create (rows*2) (cols*2) "*"
+    member this.Set((row:int),(col:int),(cont:string)) = board.[(row-1)*2,(col-1)*2] <- cont
+    
+    member this.SetRightWall((row:int),(col:int)) = board.[(row-1)*2,col*2-1] <- "|"
+    member this.SetBottomWall((row:int),(col:int)) = board.[row*2-1,(col-1)*2] <- "-"
+
     member this.Show() = 
         let mutable str = ""
-        for row=0 to rows*2 do
-            for col=0 to cols*2 do
+        for row=0 to rows*2-1 do
+            for col=0 to cols*2-1 do
                 str <- str + board.[row,col]
             str <- str + "\n"
         printfn "%s" str
 
+   
 let test1 = new BoardDisplay(3,3)
 test1.Set(1,1,"1")
+test1.SetRightWall(1,1)
+test1.SetBottomWall(1,1)
 test1.Show()
-test1.Set(2,2,"1")
+// test1.Set(1,2,"1")
+// test1.Show()
+test1.Set(2,1,"1")
+test1.SetRightWall(2,2)
 test1.Show()
-test1.Set(1,2,"1")
-test1.Show()
+
+
+
+type Direction = North | South | East | West
+type Position = int * int
+
+type Action =
+    | Stop of Position
+    | Continue of Direction * Position
+    | Ignore
+
